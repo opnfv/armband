48410b27c2ba6046e088127bb2a9f4eca6cf328e
Josep Puigdemont <josep.puigdemont@enea.com>
2016-04-20 17:42:45 +0200

    build.sh: actually build the ISO.
    Change-Id: Ica40e2fd78d6569cc17e6702a094889ae732249a
    Signed-off-by: Josep Puigdemont <josep.puigdemont@enea.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+# Copyright (c) 2016 Enea Software AB
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
 
-if [ $# -gt 1 ]; then
-    OUTPUT_DIR=shift
+error_exit() {
+    echo "$@" >&2
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    OUTPUT_DIR=$(pwd)
 else
-    OUTPUT_DIR=""
+    OUTPUT_DIR=$(readlink -f $1)
+    shift
 fi
 
+mkdir -p $OUTPUT_DIR || error_exit "Could not create directory $OUTPUT_DIR"
+
 echo "Building armband, output dir: $OUTPUT_DIR"
+cd ..
+
+SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+BUILD_BASE=$(readlink -e ${SCRIPT_DIR}/../upstream/fuel/build/)
+RESULT_DIR="${BUILD_BASE}/release"
+
+make release || error_exit "Make release failed"
+
+echo "Copy results to $OUTPUT_DIR"
+sort ${BUILD_BASE}/gitinfo*.txt > ${OUTPUT_DIR}/gitinfo.txt
+cp ${RESULT_DIR}/*.iso ${OUTPUT_DIR}/
+cp ${RESULT_DIR}/*.iso.txt ${OUTPUT_DIR}/
