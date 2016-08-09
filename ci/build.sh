5672daed23d94c6798c1a8a610b9858bb52bbbfb
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2016-08-09 18:40:22 +0200

    build: ISO refactor, use docker, enable cache
    Previously, Armband Makefile used to call Fuel@OPNFV Makefile
    target <iso>, instead of <all>, which resulted in bypassing
    using Docker for ISO building completely.
    
    Switch to <all> Makefile target, hence bringing back Docker
    as a build container.
    
    This change justifies moving make variables to a separate spec
    file, armband.mk.
    
    While at it, enable caching of builds.
    This requires Jenkins bash scripts to be updated for the ISO
    build job.
    
    JIRA: ARMBAND-56
    
    Change-Id: I7d73409be44983fa21d4c09d2e07b87a33231e03
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- ci/build.sh
+++ ci/build.sh
@@ -5,47 +5,18 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 
-error_exit() {
-    echo "$@" >&2
-    exit 1
-}
-
 write_gitinfo() {
     git_url=$(git config --get remote.origin.url)
     git_rev=$(git rev-parse HEAD)
     echo "$git_url: $git_rev"
 }
 
-if [ $# -eq 0 ]; then
-    OUTPUT_DIR=$(pwd)
-else
-    OUTPUT_DIR=$(readlink -f $1)
-    shift
-fi
-
-mkdir -p $OUTPUT_DIR || error_exit "Could not create directory $OUTPUT_DIR"
-
-echo "Building armband, output dir: $OUTPUT_DIR"
-cd ..
-
 SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
-BUILD_BASE="${SCRIPT_DIR}/upstream/fuel/build"
-RESULT_DIR="${BUILD_BASE}/release"
-
-make REVSTATE="${OPNFV_ARTIFACT_VERSION}" release ||
-    error_exit "Make release failed"
+BUILD_BASE=$(readlink -e ${SCRIPT_DIR}/../upstream/fuel/build)
+ARMBAND_BASE=$(readlink -e ${SCRIPT_DIR}/..)
 
 write_gitinfo >> ${BUILD_BASE}/gitinfo_armband.txt
 
-echo "Moving results to $OUTPUT_DIR"
-sort ${BUILD_BASE}/gitinfo*.txt > ${OUTPUT_DIR}/gitinfo.txt
-mv ${RESULT_DIR}/*.iso ${OUTPUT_DIR}/
-mv ${RESULT_DIR}/*.iso.txt ${OUTPUT_DIR}/
-
-# We need to build our own ODL plugin, and when this happens, fuel
-# renames the iso to unofficial-opnfv-${REVSTATE}.iso, so here we remove
-# the prefix:
-pushd ${OUTPUT_DIR} > /dev/null
-rename 's/^unofficial-//' *.iso
-rename 's/^unofficial-//' *.iso.txt
-popd > /dev/null
+# Initialize Armband git submodules & apply patches first
+make -C ${ARMBAND_BASE} submodules-clean submodules-init patches-import
+cd ${ARMBAND_BASE}/upstream/fuel/ci && ./build.sh $*
