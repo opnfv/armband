10bedc1b95ee97cac4a104436d8847faaaf4949e
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-10-05 03:46:54 +0200

    ci/deploy.sh: Drop local_env support
    While at it, fix shellcheck warnings in deploy wrapper.
    
    Change-Id: I2ddd272afe7ec16d6c594295a257eb98ee5bf5ce
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,33 +9,12 @@
 #
 set -e
 
-SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+SCRIPT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+ARMBAND_BASE=$(readlink -e "${SCRIPT_DIR}/..")
+export ARMBAND_BASE
 
-export ARMBAND_BASE=$(readlink -e ${SCRIPT_DIR}/..)
-
-cd ${WORKSPACE:-${ARMBAND_BASE}}
+cd "${WORKSPACE:-${ARMBAND_BASE}}"
 make patches-import
 
-# source local environment variables
-if ! [ -z $LAB_CONFIG_URL ]; then
-    while getopts "b:B:dfFl:L:p:s:S:T:i:he" OPTION 2>/dev/null
-    do
-        case $OPTION in
-            l) TARGET_LAB=$OPTARG;;
-            p) TARGET_POD=$OPTARG;;
-        esac
-    done
-    local_env=${LAB_CONFIG_URL}/labs/${TARGET_LAB}/${TARGET_POD}/fuel/config/local_env
-    # try to fetch this file, but don't create it if it does not exist.
-    # We add "|| true" to ignore the curl error when the file does not exist
-    echo "curl -s -f -O $local_env || true"
-    curl -s -f -O $local_env || true
-    local_env=$(basename $local_env)
-    if [ -e $local_env ]; then
-        echo "-- Sourcing local environment file"
-        source $local_env
-    fi
-fi
-
 cd upstream/fuel/ci
-./deploy.sh $@
+./deploy.sh "$@"
