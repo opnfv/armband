77e60fe7ecc572aa44e0da9e3817bd7b76078734
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-10-23 19:03:53 +0200

    ci/deploy.sh: Add support for CI_DEBUG
    While at it, use SSH_SALT global var in `cleanup_uefi`.
    
    Change-Id: I9400edde69d3aaaa1879cce0e79898c80db486db
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
    (cherry picked from commit 3d92843941184335c2f1b3e8a074c6e9f2e49571)

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 ##############################################################################
 # Copyright (c) 2017 Enea Software AB and others.
 # All rights reserved. This program and the accompanying materials
@@ -6,11 +6,12 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-set -e
-
+CI_DEBUG=${CI_DEBUG:-0}; [[ "${CI_DEBUG}" =~ (false|0) ]] || set -x
 SCRIPT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 ARMBAND_BASE=$(readlink -e "${SCRIPT_DIR}/..")
+
 export ARMBAND_BASE
+export CI_DEBUG
 
 cd "${WORKSPACE:-${ARMBAND_BASE}}"
 make patches-import
